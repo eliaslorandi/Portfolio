17b28043e7606f55463b0b1fd4068e8e6135ad22
Elias Lorandi <elias.lorandi@hotmail.com>
2023-11-10 09:53:06 -0300

    Completion of views

---
+++ controllers/HelpController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class HelpController extends Controller
+{
+    public function actionIndex(){
+        return $this->render('index');
+    }
+
+    public function actionAccountSettings(){
+        return $this->render('accountSettings');
+    }
+
+    public function actionLoginAndSecurity(){
+        return $this->render('loginAndSecurity');
+    }
+
+    public function actionPrivacity(){
+        return $this->render('privacity');
+    }
+}
